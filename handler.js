a8a4d5379f57066396af7c21a1124fadc14cdb5e
Beto Esquivel <ppbeto94@gmail.com>
2016-09-24 21:44:14 +0100

    recursive aws lambda function that controls the level of concurrency I want

--- handler.js
+++ handler.js
@@ -1,8 +1,59 @@
 'use strict';
+const AWS = require('aws-sdk');
+const bluebirdP = require('bluebird');
+
+const region = 'eu-west-1';
+AWS.config.update({region: region || 'eu-west-1'});
+
+const lambda = new AWS.Lambda();
+const CONSUMER_LAMBDA = "wefarm-entity-service-dev-process";
+
+const error = (context) => (err) => {
+  //debugger;
+  const contextString = JSON.stringify(context);
+  console.log(`Error::${contextString}\nmsg:err`);
+};
+
+function promiseTimeout(ms, no, toAccept) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+          if(toAccept) {
+            resolve();
+          }else {
+            reject(no + ': Promise timed out after ' + ms + ' ms');
+          }
+        }, ms);
+    });
+}
+
+const delegate = (ConsumerLambda) => () => {
+  //debugger;
+  const lambdaName = ConsumerLambda;
+  const params = {
+    FunctionName: ConsumerLambda,
+    Payload: ""
+  }
+  let lambdaResponse = lambda.invoke(params).promise().catch( error({
+                   info: 'error invoking lambda',
+                   payload: "Nothing"
+                 }));
+  //debugger;
+  return  lambdaResponse;
+};
 
 // Your first function handler
-module.exports.hello = (event, context, cb) => cb(null,
-  { message: 'Go Serverless v1.0! Your function executed successfully!', event }
-);
+module.exports.dispatch = (event, context, cb) => {
+  let timeout = promiseTimeout(290000, 1);
 
-// You can add more handlers here, and reference them in serverless.yml
+  let calls = new Array(1000000);
+  let dispatching = bluebirdP.map( calls, delegate(CONSUMER_LAMBDA) , {concurrency: 500} );
+
+  Promise.race([timeout, dispatching]).then(() => {
+    delegate('wefarm-job-dispatcher-dev-dispatch')();
+    cb(null,
+        { message: 'Dispatched all the calls, recurring', event })
+  }).catch((e) => {
+    delegate('wefarm-job-dispatcher-dev-dispatch')();
+    cb(e + "\n" + "Timeout, recurring");
+  });
+}
